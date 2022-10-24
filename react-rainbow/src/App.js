2bc729ae5c863742493610e8a82e19b434784a54
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-10-23 20:46:45 -0400

    react rainbow

---
+++ react-rainbow/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+//make ColorBlock import
+import ColorBlock from './ColorBlock'
+
+
+function App(){
+    let [colors,setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+        })
+
+    return (
+      <div className="App">
+      {colors.map((color, i) => 
+          <ColorBlock color={color} key={i}/>
+      )}
+    
+  </div>
+    )
+    
+}
+ 
+
+export default App;
